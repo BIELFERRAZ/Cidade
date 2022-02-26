2e3dcecf400c7bd12b618b496f763bd3e4763ccc
matheus <matheusgabrielferraz0802@gmail.com>
2022-02-25 22:39:50 -0300

    finalizar tarefa

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Santos!");
+Console.WriteLine("Peruíbe!");
+Console.WriteLine("Guarujá!");
+Console.WriteLine("Itanhaém!");
+Console.WriteLine("São Vicente");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Mongaguá");
